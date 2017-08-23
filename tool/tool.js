abea381ac715458845b8a4974cda93c3cdf36ae4
Dylan Turney <dylan@icerfish.com>
2017-08-23 12:18:47 +0100

    Updated tool

--- tool/tool.js
+++ tool/tool.js
@@ -1,4 +1,4 @@
-var areas = ['AB','AL','B','BA','BB','BD','BH','BL','BN','BR','BS','BT','CA','CB','CF','CH','CM','CO','CR','CT','CV','CW','DA','DD','DE','DG','DH','DL','DN','DT','DY','E','EC','EH','EN','EX','FK','FY','G','GL','GU','HA','HD','HG','HP','HR','HS','HU','HX','IG','IP','IV','KA','KT','KW','KY','L','LA','LD','LE','LL','LN','LS','LU','M','ME','MK','ML','N','NE','NG','NN','NP','NR','NW','OL','OX','PA','PE','PH','PL','PO','PR','RG','RH','RM','S','SA','SE','SG','SK','SL','SM','SN','SO','SP','SR','SS','ST','SW','SY','TA','TD','TF','TN','TQ','TR','TS','TW','UB','W','WA','WC','WD','WF','WN','WR','WS','WV','YO','ZE'];
+var areas = ['AB', 'AI', 'AL', 'AS', 'B', 'BA', 'BB', 'BBN', 'BD', 'BF', 'BH', 'BI', 'BL', 'BN', 'BR', 'BS', 'BT', 'BX', 'CA', 'CB', 'CF', 'CH', 'CM', 'CO', 'CR', 'CT', 'CV', 'CW', 'DA', 'DD', 'DE', 'DG', 'DH', 'DL', 'DN', 'DT', 'DY', 'E', 'EC', 'EH', 'EN', 'EX', 'FI', 'FK', 'FY', 'G', 'GIR', 'GL', 'GU', 'GX', 'GY', 'HA', 'HD', 'HG', 'HP', 'HR', 'HS', 'HU', 'HX', 'IG', 'IM', 'IP', 'IV', 'JE', 'KA', 'KT', 'KW', 'KY', 'KY', 'L', 'LA', 'LD', 'LE', 'LL', 'LN', 'LS', 'LU', 'M', 'ME', 'MK', 'ML', 'MS', 'N', 'NE', 'NG', 'NN', 'NP', 'NR', 'NW', 'OL', 'OX', 'PA', 'PC', 'PE', 'PH', 'PL', 'PO', 'PR', 'RG', 'RH', 'RM', 'S', 'SA', 'SE', 'SG', 'SI', 'SK', 'SL', 'SM', 'SN', 'SO', 'SP', 'SR', 'SS', 'ST', 'STH', 'SW', 'SY', 'TA', 'TD', 'TDC', 'TF', 'TK', 'TN', 'TQ', 'TR', 'TS', 'TW', 'UB', 'VG', 'W', 'WA', 'WC', 'WD', 'WF', 'WN', 'WR', 'WS', 'WV', 'YO', 'ZE'];
 
 var wikipediaUrl = 'https://en.wikipedia.org/w/index.php?title=Template%3AAttached_KML%2F<>_postcode_area&action=raw'
 
@@ -6,23 +6,40 @@ var request = require('superagent');
 var fs = require('fs');
 
 var tj = require('togeojson');
-var jsdom = require('jsdom').jsdom;
+var jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+var path = require('path');
+var DOMParser = require('xmldom').DOMParser;
 
 areas.forEach(function(areaName, index){
 	var fetchUrl = wikipediaUrl.replace('<>', areaName)
 	request.get(fetchUrl)
 		.end(function(err, data){
-			//if(err) throw err;
+            if(err) {
+            	console.error(areaName + " FAILED");
+                return;
+			}
 			var fileNameKML = 'out/kml/'+areaName+'.kml';
 			var fileNameJSON = 'out/json/'+areaName+'.json';
+			ensureDirectoryExistence(fileNameKML);
+			ensureDirectoryExistence(fileNameJSON);
 			fs.writeFile(fileNameKML, data.text, (err) => {
 				if(err) throw err;
-				var kml = jsdom(data.text);
-				var convertedWithStyles = tj.kml(kml, { styles: true });
+                var dom = (new DOMParser()).parseFromString(data.text, 'text/xml');
+                var convertedWithStyles = tj.kml(dom, { styles: true });
 				fs.writeFile(fileNameJSON, JSON.stringify(convertedWithStyles), (err) => {
 					if(err) throw err;
-					console.log(areaName, 'Saved');
+					// console.log(areaName, 'Saved');
 				})
 			})
 		})
 })
+
+function ensureDirectoryExistence(filePath) {
+  var dirname = path.dirname(filePath);
+  if (fs.existsSync(dirname)) {
+    return true;
+  }
+  ensureDirectoryExistence(dirname);
+  fs.mkdirSync(dirname);
+}
